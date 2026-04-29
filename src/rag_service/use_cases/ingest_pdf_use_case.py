315823fcb77fbfb69258cf529e10028c5b33dd52
Glffff <george980513@gmail.com>
2026-04-30 00:09:00 +0200

    feat: define use case for pdf ingestion, connet with domain objects and designed ports

---
+++ src/rag_service/use_cases/ingest_pdf_use_case.py
@@ -0,0 +1,28 @@
+from rag_service.domain.ports import PdfParser, Chunker, Embedder, DocumentRepository, VectorRepository
+from src.rag_service.domain.models import Document
+
+class IngestPdfUseCase:
+    def __init__(self, 
+                 pdf_parser: PdfParser, 
+                 chunker: Chunker,
+                 embedder: Embedder,
+                 document_repo: DocumentRepository,
+                 vector_repo: VectorRepository,
+                 ):
+        self.pdf_parser = pdf_parser
+        self.chunker = chunker
+        self.embedder = embedder
+        self.document_repo = document_repo
+        self.vector_repo = vector_repo
+    
+    def execute(self, pdf_path: str) -> tuple[bool, str]:
+        pages_text = self.pdf_parser.parse(pdf_path)
+        chunks = self.chunker.chunk(pages_text)
+        document = Document.from_file(pdf_path, pages_text, chunks)
+        self.document_repo.save_document(document)
+
+        for chunk in chunks:
+            embedding = self.embedder.get_embedding(chunk.text)
+            self.vector_repo.save(chunk, embedding)
+
+        return True, document.id
