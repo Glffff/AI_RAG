cde77dc2f2fbcabf5c9e2fac8c39d9b7b524bec2
Glffff <george980513@gmail.com>
2026-04-30 22:04:32 +0200

    refactor: set top_k as class param

--- src/rag_service/infrastructure/memory_vector_repo.py
+++ src/rag_service/infrastructure/memory_vector_repo.py
@@ -2,23 +2,24 @@ import numpy as np
 from rag_service.domain.ports import VectorRepository, Chunk
 
 class MemoryVectorRepository(VectorRepository):
-    def __init__(self):
-        self.vectors = []
+    def __init__(self, top_k: int = 3):
+        self._vectors = []
+        self._top_k = top_k
     
     def save(self, chunk: Chunk, embedding: list[float]) -> None:
-        self.vectors.append({
+        self._vectors.append({
             "chunk": chunk,
             "embedding": embedding
         })
     
-    def search(self, query_embedding: list[float], top_k: int) -> list[Chunk]:
+    def search(self, query_embedding: list[float]) -> list[Chunk]:
         query = np.array(query_embedding)
         score_item = []
-        for item in self.vectors:
+        for item in self._vectors:
             score = self._get_cosine_similarity(query, np.array(item["embedding"]))
             score_item.append((score, item["chunk"]))
         score_item.sort(key=lambda x: x[0], reverse=True)
-        return [chunk for _, chunk in score_item[:top_k]]
+        return [chunk for _, chunk in score_item[:self._top_k]]
     
     def _get_cosine_similarity(self, a: np.ndarray, b: np.ndarray) -> float:
         denominator = np.linalg.norm(a) * np.linalg.norm(b)
