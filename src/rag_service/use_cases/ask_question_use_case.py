073d1c9ff5a525ab045ab7bfacb46991a0e63e3e
Glffff <george980513@gmail.com>
2026-04-30 11:49:22 +0200

    feat: add use case Q&A

---
+++ src/rag_service/use_cases/ask_question_use_case.py
@@ -0,0 +1,18 @@
+from rag_service.domain.models import Question, Answer
+from rag_service.domain.ports import Embedder, VectorRepository, LLMService
+
+class AskQuestionUseCase:
+    def __init__(self,
+                 embedder: Embedder,
+                 vector_repo: VectorRepository,
+                 llm_service: LLMService,
+                 ):
+        self.embedder = embedder
+        self.vector_repo = vector_repo
+        self.llm_service = llm_service
+    
+    def execute(self, question: Question) -> Answer:
+        question_embedding = self.embedder.get_embedding(question.text)
+        relevant_chunks = self.vector_repo.search(question_embedding, top_k=5)
+        answer_text = self.llm_service.generate_answer(question, relevant_chunks)
+        return Answer(text=answer_text, used_chunks=relevant_chunks)
